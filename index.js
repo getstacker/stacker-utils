806269fa281116618e5be4cb87420650afc37d47
Joe Johnston <joe@simple10.com>
2014-11-14 10:12:08 -0800

    Move Logger to stacker-logger

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 require('coffee-script/register');
 
-// Try to require the global log singleton if set
-var log, Logger = require('./helpers/Logger');
-try {
-  log = require('log');
-  if (!log instanceof Logger)
-    log = null;
-} catch (e) {}
-if (!log) {
-  log = require('./utils/log');
-}
-
 module.exports = {
-  fs: require('./utils/fs'),
-  string: require('./utils/string'),
-  object: require('./utils/object'),
-  ps: require('./utils/ps'),
-  log: log
+  fs: require('./lib/fs'),
+  string: require('./lib/string'),
+  object: require('./lib/object'),
+  ps: require('./lib/ps')
 };
